e0e212deab041950591929c4f0889c59ea480f6f
TziporaGrinwald <100tzipora@gmail.com>
2018-08-06 12:38:37 +0300

    added js file

---
+++ Data-Pro-Test/Scripts/myJavaScript.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+
+    $('#submitEmail').click(function () {
+        $("#form").remove();
+        $('#success').show();
+
+        var phone = document.getElementById("phone").value;
+        var email = document.getElementById("email").value;
+        var name = document.getElementById("name").value;
+        var city = document.getElementById("city").value;
+        var friend = document.getElementById("friend").value;
+
+        $.ajax({
+            type: "POST",
+            url: "/Home/sendEmail",
+            data: { name: name, phone: phone, email: email, city: city },
+
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+
+            success: function (response) {
+                console.log("submit")
+            },
+            failure: function (response) {
+                alert(response.responseText);
+            },
+            error: function (response) {
+                alert(response.responseText);
+            }
+        });
+
+    });
+});
