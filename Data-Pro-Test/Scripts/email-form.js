f2b5e1a6309a2d34e01a1859ede5abb4721d3261
TziporaGrinwald <100tzipora@gmail.com>
2018-08-08 16:52:48 +0300

    Add partial views

---
+++ Data-Pro-Test/Scripts/email-form.js
@@ -0,0 +1,41 @@
+﻿
+$(document).ready(function () {
+
+    $('#submitEmail').click(function () {
+        $("#form").remove();
+        $('#success').show();
+
+        var phone = $("#phone").value();
+
+        var email = $("#email").value();
+
+        var name = $("#name").value();
+
+        var city = $("#city").value();
+
+        var phone = $("#phone").value();
+
+        var friend = $("#friend").value();
+
+
+        $.ajax({
+            type: "POST",
+            url: "/Home/sendEmail",
+            data: { name: name, phone: phone, email: email, city: city },
+
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+
+            success: function (response) {
+                console.log("submit")
+            },
+            failure: function (response) {
+                alert(response.responseText);
+            },
+            error: function (response) {
+                alert(response.responseText);
+            }
+        });
+
+    });
+});
